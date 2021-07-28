e7739865ffd8c3f2412606f3ef07a005d86a45cd
Hutomo Sakti Kartiko <hutomosaktikartiko.siskom@gmail.com>
2021-07-28 14:36:55 +0700

    Shop App

---
+++ Shop-App/lib/screens/sign_in/components/body.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/components/no_account_text.dart';
+import 'package:shop_app/screens/forgot_password.dart/forgot_password_screen.dart';
+import 'package:shop_app/screens/sign_in/components/sign_in_form.dart';
+import 'package:shop_app/components/social_card.dart';
+import 'package:shop_app/size_config.dart';
+import 'package:shop_app/constans.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding:
+              EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: SizeConfig.screenHeight * 0.02,
+                ),
+                Text(
+                  "Welcome Back",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: getProportionateScreenWidth(28),
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Sign in with your email and password \nor continue with social media",
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: SizeConfig.screenHeight * 0.06,
+                ),
+                SignInForm(),
+                SizedBox(
+                  height: SizeConfig.screenHeight * 0.05,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SocialCard(
+                      icon: "assets/icons/google-icon.svg",
+                      press: () {},
+                    ),
+                    SocialCard(
+                      icon: "assets/icons/facebook-2.svg",
+                      press: () {},
+                    ),
+                    SocialCard(
+                      icon: "assets/icons/twitter.svg",
+                      press: () {},
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: SizeConfig.screenHeight * 0.03,
+                ),
+                NoAccountText()
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
