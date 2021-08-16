dc34c8b5b371d4605c396007dde1bb2e754e2321
Hutomo Sakti Kartiko <hutomosaktikartiko.siskom@gmail.com>
2021-08-16 18:59:33 +0700

    Chat App - Main Screen

--- chat_app/lib/config/custom_color.dart
+++ chat_app/lib/config/custom_color.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class CustomColor {
   static const Color primaryColor = Color(0xFF6A6AE3);
   static const Color backgroundColor = Color(0xFF252836);
+  static const Color backgroundColor2 = Color(0xFF2F3142);
   static const Color grey1 = Color(0xFF9A9CA5);
   static const Color grey2 = Color(0xFF92949B);
   static const Color grey3 = Color(0xFF959CA4);
--- chat_app/lib/ui/screens/main_screen.dart
+++ chat_app/lib/ui/screens/main_screen.dart
@@ -1,17 +1,80 @@
+import 'package:chat_app/config/custom_color.dart';
+import 'package:chat_app/ui/pages/call_page.dart';
+import 'package:chat_app/ui/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatefulWidget {
-  const MainScreen({ Key? key }) : super(key: key);
+  const MainScreen({Key? key}) : super(key: key);
 
   @override
   _MainScreenState createState() => _MainScreenState();
 }
 
 class _MainScreenState extends State<MainScreen> {
+  int currentIndex = 0;
+  List<Widget> pages = [HomePage(), CallPage()];
+
+  List<BottomNavigationBarItem> bottomNavigatorBarItem = [
+    BottomNavigationBarItem(
+        icon: Padding(
+          padding: EdgeInsets.only(bottom: 5, top: 10),
+          child: ImageIcon(AssetImage("assets/icons/icon_chat.png")),
+        ),
+        label: "Chats"),
+    BottomNavigationBarItem(
+        icon: Padding(
+          padding: EdgeInsets.only(bottom: 5, top: 10),
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              ImageIcon(AssetImage("assets/icons/icon_call.png")),
+              Positioned(
+                left: -3,
+                top: 3,
+                child: Container(
+                  height: 8,
+                  width: 8,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: CustomColor.red,
+                    border: Border.all(
+                      color: CustomColor.backgroundColor2
+                    )
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        label: "Calls"),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: pages[currentIndex],
+      backgroundColor: CustomColor.backgroundColor,
+      bottomNavigationBar: buildBottomNavigatorBar(),
     );
+  }
+
+  BottomNavigationBar buildBottomNavigatorBar() {
+    return BottomNavigationBar(
+      backgroundColor: CustomColor.backgroundColor2,
+      currentIndex: currentIndex,
+      onTap: moveTab,
+      items: bottomNavigatorBarItem,
+      selectedItemColor: CustomColor.primaryColor,
+      unselectedItemColor: CustomColor.grey2,
+      iconSize: 27,
+      unselectedFontSize: 12,
+      selectedFontSize: 12,
+    );
+  }
+
+  void moveTab(int index) {
+    setState(() {
+      currentIndex = index;
+    });
   }
 }
