3bdb83813e8477c69815096668c469c7e613726b
Hutomo Sakti Kartiko <hutomosaktikartiko.siskom@gmail.com>
2021-08-16 18:10:42 +0700

    Chat App - Add Assets and Config

---
+++ chat_app/lib/config/custom_color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static const Color primaryColor = Color(0xFF6A6AE3);
+  static const Color backgroundColor = Color(0xFF252836);
+  static const Color grey1 = Color(0xFF9A9CA5);
+  static const Color grey2 = Color(0xFF92949B);
+  static const Color grey3 = Color(0xFF959CA4);
+  static const Color red = Color(0xFFE55E5E);
+  static const Color blue = Color(0xFF3988FC);
+  static const Color green = Color(0xFF33BA00);
+}
---
+++ chat_app/lib/config/custom_text_style.dart
@@ -0,0 +1,10 @@
+import 'package:chat_app/config/custom_color.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextStyle {
+  static TextStyle whiteTextStyle = TextStyle(color: Colors.white);
+  static TextStyle blackTextStyle = TextStyle(color: Colors.black);
+  static TextStyle grey1TextStyle = TextStyle(color: CustomColor.grey1);
+  static TextStyle grey2TextStyle = TextStyle(color: CustomColor.grey2);
+  static TextStyle grey3TextStyle = TextStyle(color: CustomColor.grey3);
+}
