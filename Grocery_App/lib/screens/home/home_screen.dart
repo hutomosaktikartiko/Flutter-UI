77a8b76bd33de77d2b86fcdf17c0b607c072aa2c
Hutomo Sakti Kartiko <hutomosaktikartiko.siskom@gmail.com>
2021-09-04 14:15:09 +0700

    Grocery_App - Home Page

--- Grocery_App/lib/screens/home/home_screen.dart
+++ Grocery_App/lib/screens/home/home_screen.dart
@@ -14,39 +14,62 @@ class HomeScreen extends StatelessWidget {
         bottom: false,
         child: Container(
           color: Color(0xFFEAEAEA),
-          child: Column(
-            children: [
-              HomeHeader(),
-              Expanded(
-                child: Container(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: defaultPadding),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: const BorderRadius.only(
-                      bottomLeft: Radius.circular(defaultPadding * 1.5),
-                      bottomRight: Radius.circular(defaultPadding * 1.5),
+          child: LayoutBuilder(
+            builder: (context, BoxConstraints constraints) {
+              return Stack(
+                children: [
+                  Positioned(
+                      top: 0,
+                      right: 0,
+                      left: 0,
+                      height: headerHeight,
+                      child: HomeHeader()),
+                  Positioned(
+                    top: headerHeight,
+                    left: 0,
+                    right: 0,
+                    height: constraints.maxHeight - headerHeight - cartBarHeight,
+                    child: Container(
+                      padding:
+                          const EdgeInsets.symmetric(horizontal: defaultPadding),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: const BorderRadius.only(
+                          bottomLeft: Radius.circular(defaultPadding * 1.5),
+                          bottomRight: Radius.circular(defaultPadding * 1.5),
+                        ),
+                      ),
+                      child: GridView.builder(
+                        itemCount: demo_products.length,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          childAspectRatio: 0.75,
+                          mainAxisSpacing: defaultPadding,
+                          crossAxisSpacing: defaultPadding,
+                        ),
+                        itemBuilder: (context, index) => ProductCard(
+                          product: demo_products[index],
+                          press: () {},
+                        ),
+                      ),
                     ),
                   ),
-                  child: GridView.builder(
-                    itemCount: demo_products.length,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      childAspectRatio: 0.75,
-                      mainAxisSpacing: defaultPadding,
-                      crossAxisSpacing: defaultPadding,
-                    ),
-                    itemBuilder: (context, index) => ProductCard(
-                      product: demo_products[index],
-                      press: () {},
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: cartBarHeight,
-              )
-            ],
+                  Positioned(
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    height: cartBarHeight,
+                    child: GestureDetector(
+                      onVerticalDragUpdate: (details) {
+                        print(details.primaryDelta);
+                      },
+                      child: Container(
+                        color: Colors.red,
+                      ),
+                    ))
+                ],
+              );
+            }
           ),
         ),
       ),
